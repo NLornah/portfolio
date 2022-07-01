71be80f020b2d2a3c02f3b9a599fa24e74b51eea
NLornah <nderitulornah040@gmail.com>
2022-07-01 18:22:24 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+function loading() {
+    document.querySelectorAll(".bar").forEach(function(current) {
+      let startWidth = 0;
+      const endWidth = current.dataset.size;
+      
+      const interval = setInterval(frame, 20);
+  
+      function frame() {
+        if (startWidth >= endWidth) {
+          clearInterval(interval);
+        } else {
+            startWidth++;
+            current.style.width = `${endWidth}%`;
+            current.firstElementChild.innerText = `${startWidth}%`;
+          }
+       }
+    });
+  }
+  
+  setTimeout(loading, 1000);
